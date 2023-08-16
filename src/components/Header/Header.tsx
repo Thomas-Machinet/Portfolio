f8132f34816bdf030e0601e8d8b3642502129db6
Thomas Machinet <thomasmachinet@gmail.com>
2023-08-16 15:25:11 +0200

    navbar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,26 @@
 import './Header.scss';
+import { useState } from 'react';
 
 function Header() {
+
+    const [showLinks, setShowLinks] = useState(false)
+
+    const handleShowLinks = () => {
+        setShowLinks(!showLinks)
+    }
     return (
-        <ul className='header_nav'>
-            <li className='header_nav-item'>Skills</li>
-            <li className='header_nav-item'>Projects</li>
-            <li className='header_nav-item'>Contact</li>
-        </ul>
+        <nav className={`header__navbar ${showLinks ? "show-nav" : "hide-nav"}`}>
+            <div className='header__navbar-logo'>Logo</div>
+            <ul className='header__navbar-links'>
+                <li className='header__navbar-links-item'>Skills</li>
+                <li className='header__navbar-links-item'>Projects</li>
+                <li className='header__navbar-links-item'>Contact</li>
+            </ul>
+            <button className='header__navbar-burger'
+                onClick={handleShowLinks}>
+                <span className="header__navbar-burger-bar"></span>
+            </button>
+        </nav>
     )
 }
 
