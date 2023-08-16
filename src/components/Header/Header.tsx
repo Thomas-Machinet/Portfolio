2a0791d5d1dd7a2a4b6e980ca5608f34b85d3c41
Thomas Machinet <thomasmachinet@gmail.com>
2023-08-16 13:35:15 +0200

    initial project version

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import './Header.scss';
+
+function Header() {
+    return (
+        <ul className='header_nav'>
+            <li className='header_nav-item'>Skills</li>
+            <li className='header_nav-item'>Projects</li>
+            <li className='header_nav-item'>Contact</li>
+        </ul>
+    )
+}
+
+export default Header;
