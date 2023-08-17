c83688c03d6a0507cd1f4b4697764bf79801f904
Thomas Machinet <thomasmachinet@gmail.com>
2023-08-17 10:10:27 +0200

    Navbar mobile done

--- src/App.tsx
+++ src/App.tsx
@@ -10,27 +10,7 @@ function App() {
     <>
       <Header />
       <main className="main">
-        <h1>Accueil</h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, illum vel. Asperiores placeat totam sit laborum nesciunt iste eaque corporis aspernatur maiores numquam aperiam veniam facere reprehenderit, eius nisi enim.
-          Optio eaque culpa vitae molestiae. Animi similique aut explicabo dicta modi expedita quaerat velit. Voluptas voluptatem, corporis suscipit similique dignissimos itaque cupiditate amet animi ducimus! Iusto mollitia consequatur atque deserunt.
-          Similique consequuntur ipsum temporibus nam enim? Expedita, veniam aliquid odit laborum totam ex accusamus, excepturi iusto error a ab fugiat, dignissimos corrupti distinctio exercitationem modi optio. Minima, at veritatis. Voluptatum.
-          Earum fugiat officia, illum numquam quibusdam tempora adipisci repellat quo asperiores et ea officiis fuga alias tempore nemo debitis quos possimus natus deleniti? Recusandae eum, molestiae quas dicta tempora iusto.
-          Tempore architecto, culpa nostrum provident, nesciunt obcaecati illo error velit impedit rerum tenetur fuga consequuntur. Soluta labore doloremque fuga, magni dolor itaque corrupti, aliquid maxime, laudantium tempore minima. Commodi, sunt!</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, illum vel. Asperiores placeat totam sit laborum nesciunt iste eaque corporis aspernatur maiores numquam aperiam veniam facere reprehenderit, eius nisi enim.
-          Optio eaque culpa vitae molestiae. Animi similique aut explicabo dicta modi expedita quaerat velit. Voluptas voluptatem, corporis suscipit similique dignissimos itaque cupiditate amet animi ducimus! Iusto mollitia consequatur atque deserunt.
-          Similique consequuntur ipsum temporibus nam enim? Expedita, veniam aliquid odit laborum totam ex accusamus, excepturi iusto error a ab fugiat, dignissimos corrupti distinctio exercitationem modi optio. Minima, at veritatis. Voluptatum.
-          Earum fugiat officia, illum numquam quibusdam tempora adipisci repellat quo asperiores et ea officiis fuga alias tempore nemo debitis quos possimus natus deleniti? Recusandae eum, molestiae quas dicta tempora iusto.
-          Tempore architecto, culpa nostrum provident, nesciunt obcaecati illo error velit impedit rerum tenetur fuga consequuntur. Soluta labore doloremque fuga, magni dolor itaque corrupti, aliquid maxime, laudantium tempore minima. Commodi, sunt!</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, illum vel. Asperiores placeat totam sit laborum nesciunt iste eaque corporis aspernatur maiores numquam aperiam veniam facere reprehenderit, eius nisi enim.
-          Optio eaque culpa vitae molestiae. Animi similique aut explicabo dicta modi expedita quaerat velit. Voluptas voluptatem, corporis suscipit similique dignissimos itaque cupiditate amet animi ducimus! Iusto mollitia consequatur atque deserunt.
-          Similique consequuntur ipsum temporibus nam enim? Expedita, veniam aliquid odit laborum totam ex accusamus, excepturi iusto error a ab fugiat, dignissimos corrupti distinctio exercitationem modi optio. Minima, at veritatis. Voluptatum.
-          Earum fugiat officia, illum numquam quibusdam tempora adipisci repellat quo asperiores et ea officiis fuga alias tempore nemo debitis quos possimus natus deleniti? Recusandae eum, molestiae quas dicta tempora iusto.
-          Tempore architecto, culpa nostrum provident, nesciunt obcaecati illo error velit impedit rerum tenetur fuga consequuntur. Soluta labore doloremque fuga, magni dolor itaque corrupti, aliquid maxime, laudantium tempore minima. Commodi, sunt!</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, illum vel. Asperiores placeat totam sit laborum nesciunt iste eaque corporis aspernatur maiores numquam aperiam veniam facere reprehenderit, eius nisi enim.
-          Optio eaque culpa vitae molestiae. Animi similique aut explicabo dicta modi expedita quaerat velit. Voluptas voluptatem, corporis suscipit similique dignissimos itaque cupiditate amet animi ducimus! Iusto mollitia consequatur atque deserunt.
-          Similique consequuntur ipsum temporibus nam enim? Expedita, veniam aliquid odit laborum totam ex accusamus, excepturi iusto error a ab fugiat, dignissimos corrupti distinctio exercitationem modi optio. Minima, at veritatis. Voluptatum.
-          Earum fugiat officia, illum numquam quibusdam tempora adipisci repellat quo asperiores et ea officiis fuga alias tempore nemo debitis quos possimus natus deleniti? Recusandae eum, molestiae quas dicta tempora iusto.
-          Tempore architecto, culpa nostrum provident, nesciunt obcaecati illo error velit impedit rerum tenetur fuga consequuntur. Soluta labore doloremque fuga, magni dolor itaque corrupti, aliquid maxime, laudantium tempore minima. Commodi, sunt!</p>
+
       </main>
     </>
   )
