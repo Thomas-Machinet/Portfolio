150ef84e732d2e23ec8e3c0accb5b0d79a0be668
Thomas Machinet <thomasmachinet@gmail.com>
2023-08-18 15:25:05 +0200

    Portrait done

--- src/components/Portrait/Portrait.tsx
+++ src/components/Portrait/Portrait.tsx
@@ -1,14 +1,36 @@
 import face from '../../assets/20230818_095300-removebg-preview.png'
+import { GitHub, Linkedin } from 'react-feather';
 import './Portrait.scss'
 
 function Portrait() {
     return (
-        <section className="portrait">
-            <img className="portrait__face" src={face} alt='face'></img>
-            <p className="portrait__resume"><span className="portrait__resume-intro">Hello there! I'm</span>
-                <span className="portrait__resume-name">Thomas Machinet</span>After training in the field of Web development, I gained numerous skills and knowledge through projects carried out in various programming languages.
-                Aware of the path ahead to enhance my skills, I am more motivated than ever to learn and improve.</p>
-        </section>
+        <>
+            <section className="portrait">
+                <img className="portrait__face" src={face} alt='face'></img>
+                <p className="portrait__resume"><span className="portrait__resume-intro">Hello there! I'm</span>
+                    <span className="portrait__resume-name">Thomas Machinet</span>After training in the field of Web development, I gained numerous skills and knowledge through projects carried out in various programming languages.
+                    Aware of the path ahead to enhance my skills, I am more motivated than ever to learn and improve.</p>
+            </section>
+            <div className="portrait__link">
+
+                <a
+                    href="https://www.linkedin.com/in/thomas-machinet-71201926a"
+                    className="portrait__link-item"
+                    target='_blank'>
+                    <Linkedin />
+                    Linkedin
+                </a>
+                <a
+                    href="https://github.com/Thomas-Machinet"
+                    className="portrait__link-item"
+                    target='_blank'>
+                    <GitHub />
+                    GitHub
+                </a>
+
+
+            </div>
+        </>
 
     )
 }
