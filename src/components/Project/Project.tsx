a8b2198528de206da2935736341f763c4fec95c4
Thomas Machinet <thomasmachinet@gmail.com>
2023-08-26 10:49:04 +0200

    Title project toDo

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -10,6 +10,7 @@ function Project() {
                     <a href="https://github.com/O-clock-Quasar/projet-09-quiz-o-tron-front"
                         className="project__link"
                         target='_blank'>
+                        <h3 className="project__item-title">Game'O'Quiz</h3>
                         <img src={project} alt="project" className="project__item-image" />
                         <p className="project__item-resume">Game'O'Quiz is a website that compiles several quizzes, allowing users to complete them and obtain a score upon completion, Project carried out as part of the assessment for my web developer training</p>
                         <p className="project__item-techno">
@@ -23,6 +24,7 @@ function Project() {
                     <a href="https://github.com/O-clock-Quasar/projet-09-quiz-o-tron-front"
                         className="project__link"
                         target='_blank'>
+                        <h3 className="project__item-title">Game'O'Quiz</h3>
                         <img src={project} alt="project" className="project__item-image" />
                         <p className="project__item-resume">Game'O'Quiz is a website that compiles several quizzes, allowing users to complete them and obtain a score upon completion, Project carried out as part of the assessment for my web developer training</p>
                         <p className="project__item-techno">
@@ -32,6 +34,24 @@ function Project() {
                         </p>
                     </a>
                 </div>
+
+                <div className="project__item">
+                    <a href="https://github.com/O-clock-Quasar/projet-09-quiz-o-tron-front"
+                        className="project__link"
+                        target='_blank'>
+                        <h3 className="project__item-title">Game'O'Quiz</h3>
+                        <img src={project} alt="project" className="project__item-image" />
+
+                        <p className="project__item-resume">Game'O'Quiz is a website that compiles several quizzes, allowing users to complete them and obtain a score upon completion, Project carried out as part of the assessment for my web developer training</p>
+                        <p className="project__item-techno">
+                            <span className="project__item-techno-react">React</span>
+                            <span className="project__item-techno-typescript">Typescript</span>
+                            <span className="project__item-techno-redux">Redux</span>
+                        </p>
+                    </a>
+                </div>
+
+
             </section>
         </>
     )
