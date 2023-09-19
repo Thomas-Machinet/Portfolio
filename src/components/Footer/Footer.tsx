9e3cbc40898c430332370eba0d5d441f10ac6723
Thomas Machinet <thomasmachinet@gmail.com>
2023-09-19 13:30:58 +0200

    (FEAT Footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import './Footer.scss';
+
+function Footer() {
+    return (
+        <>
+            <div className="footer-line"></div>
+            <ul className="footer__info">
+                <li className="footer__info-item">Designed and built by Thomas Machinet</li>
+                <li className="footer__info-item">©Copyright 2023 - Thomas Machinet</li>
+            </ul>
+        </>
+    )
+}
+
+export default Footer;
