1f4d017239efdf04b6f408cde06a52a707ca3a01
Thomas Machinet <thomasmachinet@gmail.com>
2023-09-25 14:48:14 +0200

    FEAT New scroll animation

--- src/App.tsx
+++ src/App.tsx
@@ -10,64 +10,29 @@ import Project from './components/Project/Project';
 import Skill from './components/Skills/Skill.js';
 import Contact from './components/Contact/Contact.js';
 import Footer from './components/Footer/Footer.js';
-import { Animator, Fade, FadeIn, FadeOut, MoveIn, MoveOut, ScrollContainer, ScrollPage, Sticky, StickyOut, batch } from 'react-scroll-motion';
-import { Route, Routes } from 'react-router-dom';
+import { Reveal } from './utils/Reveal.js';
+
+
+
 
 
 
 function App() {
 
-  const isMobile = window.innerWidth <= 768;
+
 
   return (
     <>
       <Header />
-      {!isMobile &&
-        <main className="main">
-          <ScrollContainer>
-            <ScrollPage>
-              <Animator animation={batch(Fade(), Sticky(50, 45), MoveOut(-1500, 0))}>
-                <Portrait />
-              </Animator>
-            </ScrollPage>
-            <ScrollPage>
-              <br></br>
-            </ScrollPage>
-            <ScrollPage>
-              <Animator animation={batch(Fade(), Sticky(), MoveIn(-1000, -0), MoveOut(0, 1000))}>
-                <Project data={data} />
-              </Animator>
-            </ScrollPage>
-            <ScrollPage>
-              <br></br>
-            </ScrollPage>
-
-            <ScrollPage>
-              <Animator animation={batch(Fade(), Sticky(), MoveIn(1000, 0), MoveOut(-1000, 0))}>
-                <Skill skillFrontEnd={skillFrontEnd} />
-              </Animator>
-            </ScrollPage>
-            <ScrollPage>
-              <br></br>
-            </ScrollPage>
-
-            <ScrollPage>
-              <Animator animation={batch(FadeIn(), Sticky(), MoveIn(0, -1000))}>
-                <Contact />
-              </Animator>
-            </ScrollPage>
-          </ScrollContainer>
-        </main >
-      }
-      {
-        isMobile &&
-        <main className="main">
-          <Portrait />
+      <main className="main">
+        <Portrait />
+        <Reveal>
           <Project data={data} />
-          <Skill skillFrontEnd={skillFrontEnd} />
-          <Contact />
-        </main>
-      }
+        </Reveal>
+
+        <Skill skillFrontEnd={skillFrontEnd} />
+        <Contact />
+      </main >
       <footer className="footer">
         <Footer />
       </footer>
--- src/components/Portrait/Portrait.tsx
+++ src/components/Portrait/Portrait.tsx
@@ -3,43 +3,59 @@ import faceDesktop from '../../../public/Design_sans_titre-removebg.png'
 import CV from '../../../public/CV.pdf'
 import { GitHub, Linkedin, Download } from 'react-feather';
 import './Portrait.scss'
+import { motion } from 'framer-motion';
 
 function Portrait() {
     return (
         <>
-            <h2 className="title">WHO I'M I ?</h2>
-            <section className="portrait">
+            <motion.h2
+                animate={{ x: [-1500, 0] }}
+                transition={{
+                    ease: "linear",
+                    duration: 2,
+                    x: { duration: 1 }
+                }}
+                className="title">WHO I'M I ?</motion.h2>
+            <motion.section
+                className="portrait"
+                animate={{ x: [1500, 0] }}
+                transition={{
+                    ease: "linear",
+                    duration: 2,
+                    x: { duration: 1 }
+                }}>
+
 
                 <img className="portrait__face" src={face} alt='face'></img>
                 <img className="portrait__face-desktop" src={faceDesktop} alt='face'></img>
                 <p className="portrait__resume"><span className="portrait__resume-intro">Hello there! I'm</span>
                     <span className="portrait__resume-name">Thomas Machinet</span>After training in the field of Web development, I gained numerous skills and knowledge through projects carried out in various programming languages.
                     Aware of the path ahead to enhance my skills, I am more motivated than ever to learn and improve.</p>
-                <div className="portrait__link">
+            </motion.section>
+            <div className="portrait__link">
 
-                    <a
-                        href="https://www.linkedin.com/in/thomas-machinet-71201926a"
-                        className="portrait__link-item"
-                        target='_blank'>
-                        <Linkedin color='#453948'
-                            size='30px' />
-                        Linkedin
-                    </a>
-                    <a
-                        href="https://github.com/Thomas-Machinet"
-                        className="portrait__link-item"
-                        target='_blank'>
-                        <GitHub color='#453948'
-                            size='30px' />
-                        GitHub
-                    </a>
-                    <a href={CV} className="portrait__link-item" download>
-                        <Download color='#453948'
-                            size='30px' />CV</a>
+                <a
+                    href="https://www.linkedin.com/in/thomas-machinet-71201926a"
+                    className="portrait__link-item"
+                    target='_blank'>
+                    <Linkedin color='#453948'
+                        size='30px' />
+                    Linkedin
+                </a>
+                <a
+                    href="https://github.com/Thomas-Machinet"
+                    className="portrait__link-item"
+                    target='_blank'>
+                    <GitHub color='#453948'
+                        size='30px' />
+                    GitHub
+                </a>
+                <a href={CV} className="portrait__link-item" download>
+                    <Download color='#453948'
+                        size='30px' />CV</a>
 
 
-                </div>
-            </section>
+            </div>
         </>
 
     )
