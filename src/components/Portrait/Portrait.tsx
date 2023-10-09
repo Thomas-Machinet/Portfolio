2c8aa8784f023df61c803feab74ff31e8ec34d75
Thomas Machinet <thomasmachinet@gmail.com>
2023-10-09 10:27:47 +0200

    Fix image

--- src/components/Portrait/Portrait.tsx
+++ src/components/Portrait/Portrait.tsx
@@ -15,7 +15,7 @@ function Portrait() {
                     duration: 2,
                     x: { duration: 1 }
                 }}
-                className="title">WHO I'M I ?</motion.h2>
+                className="title">WHO I'M I?</motion.h2>
             <motion.section
                 className="portrait"
                 animate={{ x: [1500, 0], opacity: [0, 1] }}
