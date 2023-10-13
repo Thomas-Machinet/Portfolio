56619eff2df3c21a0579660056099c75e4e8463f
Thomas Machinet <thomasmachinet@gmail.com>
2023-10-13 17:15:00 +0200

    Deploy

--- src/skillFrontEnd.ts
+++ src/skillFrontEnd.ts
@@ -1,38 +1,45 @@
+import react from "../public/react.png";
+import sass from "../public/sass.png";
+import typescript from "../public/typescript.avif";
+import nodeJS from "../public/nodeJS.webp";
+import postgreSQL from "../public/postgreSQL.png";
+import express from "../public/express.png";
+
 const techno = [{
     id: 1,
     name: 'React',
-    link: '/react.png',
+    link: react,
     color: '#00CCFF',
 },
 {
     id: 2,
     name: 'Sass',
-    link: '/sass.png',
+    link: sass,
     color: '#CD669A',
 
 },
 {
     id: 3,
     name: 'TypeScript',
-    link: '/typescript.avif',
+    link: typescript,
     color: '#0078CF',
 },
 {
     id: 4,
     name: 'NodeJs',
-    link: '/nodeJS.webp',
+    link: nodeJS,
     color: '#82CC28',
 },
 {
     id: 5,
     name: 'PostgreSQL',
-    link: '/postgreSQL.png',
+    link: postgreSQL,
     color: '#336791',
 },
 {
     id: 6,
     name: 'Express',
-    link: '/express.png',
+    link: express,
     color: '#000000',
 },
 ]
